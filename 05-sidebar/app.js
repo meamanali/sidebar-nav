33fe00cf1e95dd4de538d21832220778b1c6f89d
Aman Ali <54176708+mrohan519346@users.noreply.github.com>
2022-02-21 20:32:16 +0530

    Add files via upload

---
+++ 05-sidebar/app.js
@@ -0,0 +1,20 @@
+const toggleBtn = document.querySelector('.sidebar-toggle')
+const closeBtn = document.querySelector('.close-btn')
+const sidebar = document.querySelector('.sidebar')
+
+toggleBtn.addEventListener('click', function(){
+    // console.log(sidebar.classList);
+    sidebar.classList.toggle('show-sidebar')
+
+    // if(sidebar.classList.contains('show-sidebar')){
+    //     sidebar.classList.remove('show-sidebar')
+    // }else{
+    //     sidebar.classList.add('show-sidebar')
+    // }
+    // console.log(sidebar.classList);
+})
+
+closeBtn.addEventListener('click', function(){
+    sidebar.classList.remove('show-sidebar')
+})
+
